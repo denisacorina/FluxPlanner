efaeddc944639543c41cbd2667570274f33ccbf7
Obreja <denisa.obreja.ext@siemens.com>
2023-09-11 11:33:16 +0300

    Created User repository and interface

--- FluxPlanner/FluxPlanner/Repository/UserRepository.cs
+++ FluxPlanner/FluxPlanner/Repository/UserRepository.cs
@@ -1,5 +1,7 @@
 ﻿using FluxPlanner.Data;
 using FluxPlanner.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FluxPlanner.Repository
 {
@@ -10,9 +12,37 @@ namespace FluxPlanner.Repository
         {
             _context = context;
         }
-        public IEnumerable<User> GetUsers()
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            return _context.Users.ToList();
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id) ?? throw new ArgumentNullException("User not found");
+            return user;
+        }
+
+        public async Task CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync(); 
+        }
+
+        public async Task DeleteUser(int id)
+        {
+            if (_context.Users == null)
+            {
+                throw new ArgumentNullException("User not found");
+            }
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                throw new ArgumentNullException("User not found");
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
         }
     }
 }
